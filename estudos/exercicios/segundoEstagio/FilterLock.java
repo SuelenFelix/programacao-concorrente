42a3ecb74ca728b922918060ca8769fddaa7c93c
SuelenFelix <suelensamarasf@gmail.com>
2025-03-09 11:25:48 -0300

    chore: adiciona mais detalhes em FilterLock

--- estudos/exercicios/segundoEstagio/FilterLock.java
+++ estudos/exercicios/segundoEstagio/FilterLock.java
@@ -28,13 +28,16 @@ public class FilterLock implements Lock {
 
             for (int k = 0; k < n; k++) {
                 /*
-                 * verica se existe um k, tal que o nível dele esteja acima ou igual ao id de
-                 * me, além de garantir que exista uma outra vitima que não seja me
+                 * verica se existe um k, tal que o nível dele esteja acima ou igual ao nivel de
+                 * me, além de garantir que se me for a vitima, ela deve esperar, já que está
+                 * cedendo a vez
                  */
                 while (k != me && nivel[k] >= nivel[me] && victim[j] == me) {
                     /*
                      * Esse bloco garante que o algortimo respeite os níveis, fazendo com que
-                     * threads com nivel mais alto e que não são a vítima executem
+                     * threads com nivel mais alto e que não são a vítima executem, caso haja perda
+                     * de cpu, o for mais externo garante que haja mudanças de niveis, os aumentando
+                     * para threads que já ficaram presas no while por um temp
                      * 
                      */
                 }
